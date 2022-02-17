c1915a2cf91288af82ec853942450d68c34fd2c0
Uros <urosivanovich@gmail.com>
2022-02-17 01:33:46 -0500

    third query suckss

--- queries.sql
+++ queries.sql
@@ -16,3 +16,10 @@ select
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select 
+    o.id, companyname, lastname
+    from `order` as o
+    join customer as c
+    on o.customerid = c.id
+    join employee as e
+    on o.employeeid = e.id;
